3d7b8e1d9ac0a6d83614973be3b922a9ec5275c2
notzer0 <24914325+i386sh@users.noreply.github.com>
2021-02-25 12:53:14 +1100

    Here's the actual tool itself.

---
+++ LinkTool/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace LinkTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(183, 82);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "LinkTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LinkTool/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace LinkTool
+{
+    public partial class Form1 : Form
+    {
+        public int devnum = -999;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int a = WaveIn.DeviceCount;
+            for (int i = 0; i < a; i++)
+            {
+                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(i);
+                if(deviceInfo.ProductName.Contains("Oculus"))
+                {
+                    devnum = i;
+                }
+
+            }
+        }
+        public WaveIn waveIn;
+        public bool rn;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(devnum == -999)
+            {
+                MessageBox.Show("Oculus Headset mic cannot be found. Are you sure you have Oculus software installed?");
+            }
+            if(rn == true)
+            {
+                waveIn.StopRecording();
+                timer1.Stop();
+                button1.Text = "Start";
+                rn = false;
+            } else
+            {
+                waveIn = new WaveIn();
+                waveIn.DeviceNumber = devnum;
+                // Set device channels and samplerate
+                int sampleRate = 48000; // 48000 Hz
+                int channels = 1; // mono
+                waveIn.WaveFormat = new WaveFormat(sampleRate, channels);
+                waveIn.StartRecording();
+                timer1.Start(); 
+                button1.Text = "Stop";
+                rn = true;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName("vrserver"); // Detect SteamVR
+            if(pname.Length > 0)
+            {
+                timer1.Stop(); 
+                waveIn.StopRecording();
+                Process.Start(@"OculusDebugToolCLI.exe", "-f \"aswoff.txt\""); // Disable Asynchronous Spacewarp (vomit)
+                Application.Exit();
+            }
+        }
+    }
+}
